270b88c37c4a39685b73dedebd30c68487db8701
Brendan C. Ward <bcward@astutespruce.com>
2020-10-23 14:19:46 -0700

    Add overviews for SLR to speedup rendering maps

--- analysis/prep/prepare_slr.py
+++ analysis/prep/prepare_slr.py
@@ -7,7 +7,7 @@ import pygeos as pg
 from pyogrio import write_dataframe
 
 from analysis.constants import DATA_CRS
-
+from analysis.lib.raster import add_overviews
 
 data_dir = Path("data/inputs")
 src_dir = data_dir / "threats/slr"
@@ -23,5 +23,11 @@ bnd = pg.union_all(boxes)
 df = gp.GeoDataFrame({"geometry": [bnd], "index": [0]}, crs=DATA_CRS)
 df.to_feather(src_dir / "slr_bounds.feather")
 
+# For debugging
 # write_dataframe(df, "/tmp/slr_bounds.gpkg", driver="GPKG")
 
+# Create overviews for each individual file in the VRT
+# Note: these have varying resolution, but this creates lower resolutions for each
+print("Adding overviews to SLR files...")
+for filename in src_dir.glob("*.tif"):
+    add_overviews(filename)
