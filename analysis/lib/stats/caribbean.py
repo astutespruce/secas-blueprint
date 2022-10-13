Fix marine summary units to exclude tiny ones, overhaul packaging of summary data for tiles

analysis/lib/stats/caribbean.py
@@ -148,9 +148,12 @@ def summarize_caribbean_by_units_grid(df, units_grid, out_dir):
             * cellsize
         )
 
+    # remove 0 bin; priorities are 1-24
+    priority_acres = priority_acres[:, 1:]
+
     priorities = pd.DataFrame(
         priority_acres,
-        columns=[f"priority_{v}" for v in bins],
+        columns=[f"priority_{v}" for v in bins[1:]],
         index=df.index,
     )
     total_acres = priority_acres.sum(axis=1)
@@ -192,7 +195,9 @@ def get_caribbean_unit_results(results_dir, unit_id, rasterized_acres):
 
     unit = car_results.iloc[0]
 
-    cols = [c for c in car_results if c.startswith("priority_")]
+    values = pluck(INPUTS[ID]["values"], ["blueprint", "value", "label"])
+
+    cols = [f"priority_{v['value']}" for v in values]
     priority_acres = unit[cols].values
     total_acres = priority_acres.sum()
 
@@ -203,9 +208,7 @@ def get_caribbean_unit_results(results_dir, unit_id, rasterized_acres):
             "acres": priority_acres[i],
             "percent": 100 * priority_acres[i] / rasterized_acres,
         }
-        for i, e in enumerate(
-            pluck(INPUTS[ID]["values"], ["blueprint", "value", "label"])
-        )
+        for i, e in enumerate(values)
         if priority_acres[i]
     ]
 

analysis/lib/stats/core.py
@@ -9,10 +9,6 @@ from analysis.constants import (
     BLUEPRINT,
     INPUTS,
     M2_ACRES,
-    DATA_CRS,
-    GEO_CRS,
-    M_MILES,
-    LTA_SEARCH_RADIUS_BINS,
 )
 from analysis.lib.geometry import to_crs
 from analysis.lib.raster import (
@@ -20,6 +16,7 @@ from analysis.lib.raster import (
     extract_count_in_geometry,
     summarize_raster_by_units_grid,
 )
+from analysis.lib.stats.ownership import get_lta_search_info
 from analysis.lib.stats.summary_units import (
     read_unit_from_feather,
     huc12_filename,
@@ -268,21 +265,6 @@ def get_unit_core_results(unit_type, unit_id):
 
     bounds = df[["minx", "miny", "maxx", "maxy"]].apply(list, axis=1)[0]
 
-    box = to_crs(np.array([pg.box(*bounds)]), GEO_CRS, DATA_CRS)[0]
-    center = [(unit.minx + unit.maxx) / 2.0, (unit.miny + unit.maxy) / 2.0]
-    extent_radius = int(
-        round(
-            pg.distance(pg.get_point(pg.get_exterior_ring(box), 0), pg.centroid(box))
-            * M_MILES
-        )
-    )
-    lta_search_radius = LTA_SEARCH_RADIUS_BINS[
-        min(
-            np.digitize(extent_radius, LTA_SEARCH_RADIUS_BINS),
-            len(LTA_SEARCH_RADIUS_BINS) - 1,
-        )
-    ]
-
     # read SE Blueprint
     blueprint_results = (
         read_unit_from_feather(results_dir / "blueprint.feather", unit_id)
@@ -316,8 +298,13 @@ def get_unit_core_results(unit_type, unit_id):
         "promote_base": unit.input_id == "base",
         "blueprint": blueprint,
         "bounds": bounds,
-        "center": center,
-        "lta_search_radius": lta_search_radius,
     }
+
+    if unit_type == "huc12":
+        center, lta_search_radius = get_lta_search_info(
+            df[["minx", "miny", "maxx", "maxy"]].values
+        )
+        results["center"] = center[0]
+        results["lta_search_radius"] = lta_search_radius[0]
 
     return df, results

analysis/lib/stats/florida_marine.py
@@ -135,9 +135,10 @@ def summarize_florida_marine_by_units_grid(df, units_grid, out_dir):
             "GeoDataFrame for summary must include value, rasterized_acres, outside_se columns"
         )
 
+    values = INPUTS[ID]["values"]
     with rasterio.open(flm_filename) as value_dataset:
         cellsize = value_dataset.res[0] * value_dataset.res[0] * M2_ACRES
-        bins = range(0, INPUTS[ID]["values"][-1]["value"] + 1)
+        bins = range(0, values[-1]["value"] + 1)
 
         priority_acres = (
             summarize_raster_by_units_grid(
@@ -145,14 +146,17 @@ def summarize_florida_marine_by_units_grid(df, units_grid, out_dir):
                 units_grid,
                 value_dataset,
                 bins=bins,
-                progress_label="Summarizing Base Blueprint",
+                progress_label="Summarizing Florida Marine Blueprint",
             )
             * cellsize
         )
 
+    # discard priority 0 column
+    priority_acres = priority_acres[:, values[0]["value"] :]
+
     priorities = pd.DataFrame(
         priority_acres,
-        columns=[f"priority_{v}" for v in bins],
+        columns=[f"priority_{v['value']}" for v in values],
         index=df.index,
     )
     total_acres = priority_acres.sum(axis=1)
@@ -194,7 +198,8 @@ def get_florida_marine_unit_results(results_dir, unit_id, rasterized_acres):
 
     unit = flm_results.iloc[0]
 
-    cols = [c for c in flm_results if c.startswith("priority_")]
+    values = pluck(INPUTS[ID]["values"], ["blueprint", "value", "label"])
+    cols = [f"priority_{v['value']}" for v in values]
 
     priority_acres = unit[cols].values
     total_acres = priority_acres.sum()
@@ -202,10 +207,10 @@ def get_florida_marine_unit_results(results_dir, unit_id, rasterized_acres):
     priorities = [
         {
             **entry,
-            "acres": priority_acres[entry["value"]],
-            "percent": 100 * priority_acres[entry["value"]] / rasterized_acres,
+            "acres": priority_acres[i],
+            "percent": 100 * priority_acres[i] / rasterized_acres,
         }
-        for entry in pluck(INPUTS[ID]["values"], ["blueprint", "value", "label"])
+        for i, entry in enumerate(values)
     ] + [
         {
             "label": "Not a priority",

analysis/lib/stats/ownership.py
@@ -1,10 +1,20 @@
 from pathlib import Path
 
 import geopandas as gp
+import numpy as np
+import pandas as pd
 import pygeos as pg
 
-from analysis.constants import M2_ACRES, OWNERSHIP, PROTECTION
-from analysis.lib.geometry import intersection
+from analysis.constants import (
+    M2_ACRES,
+    OWNERSHIP,
+    PROTECTION,
+    GEO_CRS,
+    DATA_CRS,
+    M_MILES,
+    LTA_SEARCH_RADIUS_BINS,
+)
+from analysis.lib.geometry import intersection, to_crs
 from analysis.lib.stats.summary_units import read_unit_from_feather
 
 
@@ -231,3 +241,40 @@ def get_ownership_unit_results(results_dir, unit_id, acres):
         ]
 
     return results
+
+
+def get_lta_search_info(bounds):
+    """Calculate geographic center and search radius in miles (binned to match
+    website) to search against Land Trust Alliance website.
+
+    Parameters
+    ----------
+    bounds : ndarray of shape (n, 4), in GEO_CRS
+
+    Returns
+    -------
+    ndarray of shape (n, 2), ndarray of shape (n,)
+        centers in long, lat and search distance in miles
+    """
+    boxes = to_crs(pg.box(*bounds.T), GEO_CRS, DATA_CRS)
+    centers = np.dstack(
+        [(bounds[:, 0] + bounds[:, 2]) / 2, (bounds[:, 1] + bounds[:, 3]) / 2]
+    )[0]
+
+    extent_radius = (
+        pg.distance(pg.get_point(pg.get_exterior_ring(boxes), 0), pg.centroid(boxes))
+        * M_MILES
+    )
+
+    indexes = np.clip(
+        np.digitize(extent_radius, LTA_SEARCH_RADIUS_BINS),
+        0,
+        len(LTA_SEARCH_RADIUS_BINS) - 1,
+    )
+
+    return (
+        centers,
+        pd.Series(indexes)
+        .map({i: v for i, v in enumerate(LTA_SEARCH_RADIUS_BINS)})
+        .values,
+    )

analysis/lib/stats/slr.py
@@ -8,6 +8,7 @@ import rasterio
 
 from analysis.constants import (
     M2_ACRES,
+    SLR_DEPTH_BINS,
     SLR_PROJ_COLUMNS,
     SLR_YEARS,
     SLR_PROJ_SCENARIOS,
@@ -18,9 +19,6 @@ from analysis.lib.raster import (
     summarize_raster_by_units_grid,
 )
 from analysis.lib.stats.summary_units import read_unit_from_feather
-
-
-SLR_BINS = np.arange(11)
 
 
 src_dir = Path("data/inputs/threats/slr")
@@ -87,7 +85,7 @@ def extract_slr_by_mask_and_geometry(
 
     slr_acres = (
         extract_count_in_geometry(
-            depth_filename, shape_mask, window, bins=SLR_BINS, boundless=True
+            depth_filename, shape_mask, window, bins=SLR_DEPTH_BINS, boundless=True
         )
         * cellsize
     )
@@ -173,7 +171,7 @@ def summarize_slr_by_units_grid(df, units_grid, out_dir):
                 df,
                 units_grid,
                 value_dataset,
-                bins=SLR_BINS,
+                bins=SLR_DEPTH_BINS,
                 progress_label="Summarizing SLR inundation depth",
             )
             * cellsize
@@ -191,7 +189,7 @@ def summarize_slr_by_units_grid(df, units_grid, out_dir):
 
     slr = pd.DataFrame(
         slr_acres,
-        columns=[f"depth_{v}" for v in SLR_BINS],
+        columns=[f"depth_{v}" for v in SLR_DEPTH_BINS],
         index=df.index,
     )
     slr["not_inundated"] = not_inundated_acres
@@ -265,7 +263,7 @@ def get_slr_unit_results(results_dir, unit_id, rasterized_acres):
             "acres": unit[f"depth_{i}"],
             "percent": 100 * unit[f"depth_{i}"] / rasterized_acres,
         }
-        for i in SLR_BINS
+        for i in SLR_DEPTH_BINS
     ]
 
     projections = {

api/stats/custom_area.py
@@ -4,31 +4,20 @@ import geopandas as gp
 import numpy as np
 import pygeos as pg
 
-from analysis.lib.geometry import to_dict, intersection, to_crs
-from analysis.constants import (
-    DATA_CRS,
-    GEO_CRS,
-    INPUTS,
-    M_MILES,
-    OWNERSHIP,
-    PROTECTION,
-    M2_ACRES,
-    LTA_SEARCH_RADIUS_BINS,
-)
-
-from analysis.lib.stats.core import (
-    get_shape_mask,
-    extract_input_areas_by_mask,
-    extract_blueprint_by_mask,
-)
-from analysis.lib.stats.florida_marine import extract_florida_marine_by_mask
-from analysis.lib.util import subset_dict
-
+from analysis.constants import DATA_CRS, GEO_CRS, INPUTS, M2_ACRES
+from analysis.lib.geometry import to_dict, to_crs
 from analysis.lib.stats.base_blueprint import extract_base_blueprint_by_mask
 from analysis.lib.stats.caribbean import extract_caribbean_by_mask
+from analysis.lib.stats.core import (
+    extract_blueprint_by_mask,
+    extract_input_areas_by_mask,
+    get_shape_mask,
+)
+from analysis.lib.stats.florida_marine import extract_florida_marine_by_mask
+from analysis.lib.stats.ownership import get_lta_search_info, get_ownership_for_aoi
 from analysis.lib.stats.slr import extract_slr_by_mask_and_geometry
 from analysis.lib.stats.urban import extract_urban_by_mask
-from analysis.lib.stats.ownership import get_ownership_for_aoi
+from analysis.lib.util import subset_dict
 
 data_dir = Path("data/inputs")
 boundary_filename = data_dir / "boundaries/se_boundary.feather"
@@ -50,19 +39,14 @@ def get_custom_area_results(df):
 
     geometry = df.geometry.values.data[0]
     polygon_acres = pg.area(geometry) * M2_ACRES
-    bounds = pg.bounds(geometry).tolist()
     shapes = [to_dict(geometry)]
-    center = pg.centroid(pg.box(*bounds))
-    extent_radius = int(round(pg.distance(pg.points(*bounds[0:2]), center) * M_MILES))
+    bounds = pg.bounds(geometry)
 
-    lta_search_radius = LTA_SEARCH_RADIUS_BINS[
-        min(
-            np.digitize(extent_radius, LTA_SEARCH_RADIUS_BINS),
-            len(LTA_SEARCH_RADIUS_BINS) - 1,
-        )
-    ]
+    geo_bounds = pg.bounds(to_crs(np.array([pg.box(*bounds)]), DATA_CRS, GEO_CRS))
 
-    center = pg.get_coordinates(to_crs(np.array(center), DATA_CRS, GEO_CRS)).tolist()[0]
+    center, lta_search_radius = get_lta_search_info(geo_bounds)
+    center = center[0]
+    lta_search_radius = lta_search_radius[0]
 
     # if area of interest does not intersect SE region boundary,
     # there will be no results