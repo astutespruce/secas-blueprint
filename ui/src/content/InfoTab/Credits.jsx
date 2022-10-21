8a74295d5730a39c57371516470909e6121db3f1
Brendan C. Ward <bcward@astutespruce.com>
2022-10-21 12:56:24 -0700

    Add SECAS logo, use lowercase indicator names in report captions

--- ui/src/content/InfoTab/Credits.jsx
+++ ui/src/content/InfoTab/Credits.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Box, Text } from 'theme-ui'
+import { Box, Flex, Paragraph, Image } from 'theme-ui'
 
 import { OutboundLink } from 'components/link'
+import SECASLogo from 'images/SECAS-logo-bw.svg'
 
 const Credits = () => (
   <Box as="section">
-    <Text as="p" sx={{ fontSize: 1 }}>
-      <b>Citation:</b> Southeast Conservation Adaptation Strategy (SECAS). 2022.
-      Southeast Conservation Blueprint 2022.{' '}
-      <OutboundLink to="http://secassoutheast.org/blueprint">
-        http://secassoutheast.org/blueprint
-      </OutboundLink>
-      .
-      <br />
-      <br />
+    <Flex>
+      <Box sx={{ flex: '0 0 auto' }}>
+        <Image src={SECASLogo} sx={{ width: '120px' }} />
+      </Box>
+      <Box sx={{ flex: '1 1 auto' }}>
+        <Paragraph sx={{ fontSize: 1, pl: '1rem' }}>
+          <b>Citation:</b> Southeast Conservation Adaptation Strategy (SECAS).
+          2022. Southeast Conservation Blueprint 2022.{' '}
+          <OutboundLink to="http://secassoutheast.org/blueprint">
+            http://secassoutheast.org/blueprint
+          </OutboundLink>
+          .
+        </Paragraph>
+      </Box>
+    </Flex>
+    <Paragraph sx={{ fontSize: 1, pt: '1rem' }}>
       This application was developed by{' '}
       <OutboundLink to="https://astutespruce.com">
         Astute Spruce, LLC
@@ -23,7 +31,7 @@ const Credits = () => (
         Southeast Conservation Adaptation Strategy
       </OutboundLink>
       .
-    </Text>
+    </Paragraph>
   </Box>
 )
 
