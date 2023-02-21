c90703a327832a0217d34f4919c225cd585f97db
Brendan C. Ward <bcward@astutespruce.com>
2023-02-21 12:17:25 -0800

    Increase debounce delay for map spinner

--- ui/src/components/map/Map.jsx
+++ ui/src/components/map/Map.jsx
@@ -121,7 +121,7 @@ const Map = () => {
     if (mapIsDrawingRef.current) {
       setMapIsDrawing(true)
     }
-  }, 50)
+  }, 200)
 
   const getPixelData = useDebouncedCallback(() => {
     // need to break out of pending events if the mode is different
