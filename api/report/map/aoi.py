580f778db4253e730ecf62ba891432e162cac91c
Brendan Ward <bcward@astutespruce.com>
2023-11-13 11:08:37 -0800

    Fix map styles for report

--- api/report/map/aoi.py
+++ api/report/map/aoi.py
@@ -1,22 +1,44 @@
+from base64 import b64decode
 from copy import deepcopy
+from io import BytesIO
 import json
 
 from PIL import Image
 from pymgl import Map
 
 from analysis.lib.geometry import to_dict
-
+from api.settings import TILE_DIR
 
 STYLE = {
     "version": 8,
-    "sources": {"aoi": {"type": "geojson", "data": ""}},
+    "sources": {
+        "aoi": {"type": "geojson", "data": ""},
+        "mask": {
+            "type": "vector",
+            "url": f"mbtiles://{TILE_DIR}/se_mask.mbtiles",
+        },
+    },
     "layers": [
+        {
+            "id": "mask-fill",
+            "source": "mask",
+            "source-layer": "mask",
+            "type": "fill",
+            "paint": {"fill-color": "#333333", "fill-opacity": 0.1},
+        },
+        {
+            "id": "mask-pattern",
+            "source": "mask",
+            "source-layer": "mask",
+            "type": "fill",
+            "paint": {"fill-pattern": "crosshatch", "fill-opacity": 0.25},
+        },
         {
             "id": "aoi",
             "source": "aoi",
             "type": "line",
             "paint": {"line-width": 2, "line-color": "#000000", "line-opacity": 1},
-        }
+        },
     ],
 }
 
@@ -42,19 +64,24 @@ def get_aoi_map_image(geometry, center, zoom, width, height):
     style = deepcopy(STYLE)
     style["sources"]["aoi"]["data"] = to_dict(geometry)
 
-    params = {
-        "style": style,
-        "center": center,
-        "zoom": zoom,
-        "width": width,
-        "height": height,
-    }
-
     try:
-        img_data = Map(
-            json.dumps(style), width, height, 1, *center, zoom=zoom
-        ).renderBuffer()
-        return Image.frombytes("RGBA", (width, height), img_data), None
+        map = Map(json.dumps(style), width, height, 1, *center, zoom=zoom)
+
+        # pattern generated using http://www.patternify.com/ on 10x10 grid
+        crosshatch_png = b64decode(
+            "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAADpJREFUKFONzEsKADAIA9Hk/oe2WGjpR6OzfgzRy9hwBoAVnMhnCm6k4IUy+KEIhuiFKTqhRAuWyOEA/DwKCnfY+F8AAAAASUVORK5CYII="
+        )
+        crosshatch_bytes = Image.open(BytesIO(crosshatch_png)).tobytes()
+        map.addImage(
+            "crosshatch",
+            crosshatch_bytes,
+            10,
+            10,
+            2.0,
+            False,
+        )
+
+        return Image.frombytes("RGBA", (width, height), map.renderBuffer()), None
 
     except Exception as ex:
         return None, f"Error generating aoi image ({type(ex)}): {ex}"
